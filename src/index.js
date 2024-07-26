25138d97308aee4fe50bb6612b53f7204930a22f
Sorea A <56051252+sorea19@users.noreply.github.com>
2024-07-25 20:55:50 -0700

    Fixing index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'; // Import global styles
+import App from './App'; // Import the main App component
+import reportWebVitals from './reportWebVitals'; // Import performance measurement
 
+// Render the App component into the DOM element with ID 'root'
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to measure performance in your app, you can pass a function
+// to log results (e.g., reportWebVitals(console.log))
+// or send them to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
